007da7c9e46600ab62fc606472c2c8c94865b53a
Arifur-Rahaman <arifur.rahaman.siflu@gmail.com>
2023-09-03 15:41:10 +0600

    completed layout page for smaller device

--- src/routes/layout/+page.svelte
+++ src/routes/layout/+page.svelte
@@ -1,9 +1,9 @@
-<div class="h-screen grid grid-cols-12 grid-rows-6">
-    <div class="bg-blue-400 col-start-1 col-end-3 row-start-1 row-end-4"></div>
-    <div class="bg-rose-100 col-start-3 col-end-7 row-start-1 row-end-6"></div>
-    <div class="bg-blue-400 col-start-7 col-end-13 row-start-1 row-end-3"></div>
-    <div class="bg-teal-400 col-start-1 col-end-3 row-start-4 row-end-6"></div>
-    <div class="bg-yellow-100 col-start-7 col-end-11 row-start-3 row-end-7 flex items-center justify-center">
+<div class="h-screen grid grid-cols-4 md:grid-cols-12 grid-rows-6">
+    <div class="hidden md:block bg-blue-400 col-start-1 col-end-3 row-start-1 row-end-4"></div>
+    <div class="bg-rose-100 col-span-full row-start-1 row-end-4 md:col-start-3 md:col-end-7 md:row-start-1 md:row-end-6"></div>
+    <div class="hidden md:block bg-blue-400 col-start-7 col-end-13 row-start-1 row-end-3"></div>
+    <div class="hidden md:block bg-teal-400 col-start-1 col-end-3 row-start-4 row-end-6"></div>
+    <div class="bg-yellow-100 col-span-full row-start-4 row-end-7 md:col-start-7 md:col-end-11 md:row-start-3 md:row-end-7 flex items-center justify-center">
         <div class="sticker">
             <div class="circle bg-yellow-100">
                 <div class="left-triangle"></div>
@@ -11,8 +11,8 @@
             </div>
         </div>
     </div>
-    <div class="bg-emerald-400 col-start-11 col-end-13 row-start-3 row-end-7"></div>
-    <div class="bg-fuchsia-400 col-start-1 col-end-7 row-start-6 row-end-7"></div>
+    <div class="hidden md:block bg-emerald-400 col-start-11 col-end-13 row-start-3 row-end-7"></div>
+    <div class="hidden md:block bg-fuchsia-400 col-start-1 col-end-7 row-start-6 row-end-7"></div>
 </div>
 
 <style lang="postcss">
