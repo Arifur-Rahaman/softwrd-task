73c52665259cb562a533933e167ef48035bda833
Arifur-Rahaman <arifur.rahaman.siflu@gmail.com>
2023-09-03 22:39:06 +0600

    updated map

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,21 @@
-<h1 class="text-red-600">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { goto } from "$app/navigation";
+</script>
+
+<div class="min-h-screen flex justify-center items-center">
+    <div>
+        <h1 class="text-center mb-5 text-2xl font-semibold">Welcome To The Home Page</h1>
+        <button class="bg-gray-500 text-gray-50 px-4 py-2 rounded mr-2" on:click={() => goto("/layout")}
+            >Navigate To Layout Page</button
+        >
+        <button class="bg-gray-500 text-gray-50 px-4 py-2 rounded mr-2" on:click={() => goto("/chart")}
+            >Navigate To Chart Page</button
+        >
+        <button class="bg-gray-500 text-gray-50 px-4 py-2 rounded mr-2" on:click={() => goto("/map")}
+            >Navigate To Map Page</button
+        >
+    </div>
+</div>
 
 <style lang="postcss">
-
 </style>
--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,3 +1,57 @@
-<div>
-    Map
-</div>
+<script>
+  import { onMount } from 'svelte';
+  import Map from 'ol/Map';
+  import View from 'ol/View';
+  import TileLayer from 'ol/layer/Tile';
+  import OSM from 'ol/source/OSM';
+  import VectorLayer from 'ol/layer/Vector';
+  import VectorSource from 'ol/source/Vector';
+  import GeoJSON from 'ol/format/GeoJSON';
+  import Style from 'ol/style/Style';
+  import Fill from 'ol/style/Fill';
+
+  let map;
+
+  onMount(() => {
+    const mapContainer = document.getElementById('map');
+
+    // Create a base layer with OpenStreetMap tiles
+    const baseLayer = new TileLayer({
+      source: new OSM(),
+    });
+
+    // Create a vector layer for displaying GeoJSON data
+    const vectorSource = new VectorSource({
+      format: new GeoJSON(),
+      url: 'countries.geojson',
+    });
+
+    const vectorLayer = new VectorLayer({
+      source: vectorSource,
+      style: new Style({
+        fill: new Fill({
+          color: 'rgba(0, 106, 78, 0.75)',
+        }),
+      }),
+    });
+
+    // Set up the map view
+    map = new Map({
+      layers: [baseLayer, vectorLayer],
+      target: mapContainer,
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
+  });
+</script>
+
+<style>
+  #map {
+    width: 100%;
+    height: 100vh;
+  }
+</style>
+
+<div id="map"></div>
